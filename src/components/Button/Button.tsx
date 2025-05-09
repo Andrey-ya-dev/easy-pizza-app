e9722a44c16825ffc21ab0737729fcd160bcbcdd
Andrey Ignashov <maylo7771@yandex.ru>
2025-05-09 20:07:09 +0300

    fix: Изменены Button.tsx, index.css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,22 @@
-import type { ReactNode } from "react";
+import type { ButtonHTMLAttributes } from "react";
+import clsx from "clsx";
 
-interface ButtonProps {
-  children: ReactNode;
+import cls from "./Button.module.css";
+
+type ButtonVariant = "main" | "round" | "large";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
+  variant?: ButtonVariant;
 }
-export function Button({ children, className, ...props }: ButtonProps) {
+export function Button({
+  children,
+  className,
+  variant = "main",
+  ...props
+}: ButtonProps) {
   return (
-    <button className={className} {...props}>
+    <button className={clsx(cls["button"], cls[variant], className)} {...props}>
       {children}
     </button>
   );
