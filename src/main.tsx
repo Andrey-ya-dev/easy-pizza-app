7f945729a351daf785fc89ee596893ea7318e151
Andrey Ignashov <maylo7771@yandex.ru>
2025-05-11 14:16:26 +0300

    fix: Изменено подключение router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 import "./index.css";
-
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { NotFoundPage } from "@/pages/NotFoundPage/NotFoundPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
