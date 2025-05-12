1689782f52ed582076d01c49e09bf8645c126bf7
Andrey Ignashov <maylo7771@yandex.ru>
2025-05-12 20:25:54 +0300

    fix: Input добавленны типы инпуты

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,33 +1,47 @@
-import { forwardRef } from "react";
-import type { InputHTMLAttributes } from "react";
+import type { InputHTMLAttributes, Ref } from "react";
 import clsx from "clsx";
 
 import cls from "./Input.module.css";
 
-interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   withIcon?: boolean;
+  ref?: Ref<HTMLInputElement>;
 }
 
-export const Input = forwardRef<HTMLInputElement, IInputProps>(
-  ({ className, withIcon = false, ...props }, ref) => {
-    return (
-      <div className={cls.wrapper}>
-        {withIcon && (
-          <img
-            src="/search-icon.svg"
-            alt="Поиск иконка лупы"
-            className={cls.icon}
-          />
-        )}
-        <input
-          className={clsx(cls.input, className, {
-            [cls.withIcon]: withIcon,
-          })}
-          ref={ref}
-          {...props}
+export const Input = ({
+  className,
+  withIcon = false,
+  ref,
+  ...props
+}: InputProps) => {
+  return (
+    <div className={cls.wrapper}>
+      {withIcon && (
+        <img
+          src="/search-icon.svg"
+          alt="Поиск иконка лупы"
+          className={cls.icon}
         />
-      </div>
-    );
-  }
+      )}
+      <input
+        className={clsx(cls.input, className, {
+          [cls.withIcon]: withIcon,
+        })}
+        ref={ref}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export const InputText = (props: InputProps) =>
+  Input({ ...props, type: "text" });
+
+export const InputEmail = (props: InputProps) => (
+  <Input type="email" name="email" id="email" {...props} />
+);
+
+export const InputPassword = (props: InputProps) => (
+  <Input type="password" name="password" id="password" {...props} />
 );
