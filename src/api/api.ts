82e93a65b3d1ca52012c518427b401e72fd0660e
Andrey Ignashov <maylo7771@yandex.ru>
2025-05-12 20:27:15 +0300

    feat: Добавлен sendLogin

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,6 +4,29 @@ export const getProducts = async (url: string) => {
   try {
     const res = await fetch(`${BASE_URL}/${url}`);
     const data = await res.json();
+
+    return data;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
+
+export const sendLogin = async (email: string, password: string) => {
+  try {
+    const res = await fetch(`${BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const data = await res.json();
+
     return data;
   } catch (err) {
     console.error(err);
