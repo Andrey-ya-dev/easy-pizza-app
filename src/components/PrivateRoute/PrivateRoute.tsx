38c63bb354437795e515f00866db5c5e5dce6249
Andrey Ignashov <maylo7771@yandex.ru>
2025-05-16 18:30:55 +0300

    fix: Изменена подгрузка данных с localStorage на redux state для PrivateRoute

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,8 +1,9 @@
+import { useAppSelector } from "@/store/hooks";
 import type { PropsWithChildren } from "react";
 import { Navigate } from "react-router";
 
 export function PrivateRoute({ children }: PropsWithChildren) {
-  const jwt = localStorage.getItem("jwt");
+  const jwt = useAppSelector((state) => state.user.jwt);
 
   if (!jwt) {
     return <Navigate to={"/auth/login"} replace />;
